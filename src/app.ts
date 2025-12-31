8990333219ef7161b638c4688d8fa6d54fb459da
tailison-ramos <tailisonramos427@gmail.com>
2025-12-31 02:04:02 -0300

    fix: build script adjusted

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from "express"
-import cors from "cors"
 import morgan from "morgan"
 import Routes from "./routes/routes"
+import { corsConfig } from "./config/cors.config"
 
 
 export function createApp() {
   const app = express()
 
-  app.use(cors())
+  app.use(corsConfig)
   app.use(express.json())
   app.use(morgan("dev"))
 
